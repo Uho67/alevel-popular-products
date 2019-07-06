0b87b9d1cc36b3ebb1544626aed2cfa2430b9a0f
uho0613 <uho0613@gmail.com>
2019-07-07 01:57:32 +0300

    executeFull

--- Model/Indexer/PopularProducts.php
+++ Model/Indexer/PopularProducts.php
@@ -27,13 +27,52 @@ class PopularProducts implements ActionInterface, MViewActionInterface
      */
     public function executeFull()
     {
-        $select = $this->resource->getConnection()->select();
+        $connection = $this->resource->getConnection();
+        $name ='name';
+        $small_image = 'small_image';
+        $price = 'price';
+        $is_popular = 'is_popular';
+        $pv4value = 1;
 
-        $select->from(
-            ["p" => $this->resource->getTable('catalog_product_entity')]
-        )->join([
-            'v' => $this->resource->getTable('catalog_product_entity_varchar')
-        ], 'p.entity_id = v.');
+        $select = $connection->select()->from(
+            ["p" => $this->resource->getTable('catalog_product_entity')],[])
+            ->joinInner(['pv' => $this->resource->getTable('catalog_product_entity_varchar')],
+                'pv.entity_id = p.entity_id',[])
+            ->joinInner(['pv2' => $this->resource->getTable('catalog_product_entity_varchar')],
+                'pv2.entity_id = p.entity_id',[])
+            ->joinInner(['pv3' => $this->resource->getTable('catalog_product_entity_decimal')],
+                'pv3.entity_id = p.entity_id',[])
+            ->joinRight(['pv4' => $this->resource->getTable('catalog_product_entity_int')],
+                'pv4.entity_id = p.entity_id',[])
+            ->joinInner(['ea' => $this->resource->getTable('eav_attribute')],
+                'ea.`attribute_id`= pv.`attribute_id`',[])
+            ->joinInner(['ea2' => $this->resource->getTable('eav_attribute')],
+                'ea2.`attribute_id`= pv2.`attribute_id`',[])
+            ->joinInner(['ea3' => $this->resource->getTable('eav_attribute')],
+                'ea3.`attribute_id`= pv3.`attribute_id`',[])
+            ->joinRight(['ea4' => $this->resource->getTable('eav_attribute')],
+                'ea4.`attribute_id`= pv4.`attribute_id`',[])
+
+            ->where('ea.attribute_code = ?', $name)
+            ->where('ea2.attribute_code= ?',$small_image)
+            ->where('ea3.attribute_code= ?',$price)
+            ->where('ea4.attribute_code= ?',$is_popular)
+            ->where( 'pv4.value = ?',$pv4value)
+            ->columns([
+                'final_price' => 'pv3.value',
+                'small_image' => 'pv2.value',
+                'sku' => 'p.sku',
+                'name' => 'pv.value',
+                'product_id' => 'p.entity_id',
+            ]);
+
+        $connection->query(
+            $connection->insertFromSelect(
+                $select,
+                $this->resource->getTable('alevel_popular_products'),
+                ['final_price','small_image','sku','name','product_id']
+            )
+        );
     }
 
     /**
